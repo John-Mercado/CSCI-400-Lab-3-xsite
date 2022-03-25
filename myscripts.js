b9c913bb85fa914acb750fe713309b66780a01e3
John-Mercado <102321689+John-Mercado@users.noreply.github.com>
2022-03-24 23:29:23 -0400

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,4 @@
+<script>alert('XSS');</script>
+
+
+<script> document.getElementById("").src="https://images.unsplash.com/photo-1582266255765-fa5cf1a1d501?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YW5vbnltb3VzfGVufDB8fDB8fA%3D%3D&w=1000&q=80"; </script>
